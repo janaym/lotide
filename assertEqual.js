bf5fd8c79a7b68b778c8ded4dd1801aa4fcf9e79
Janay Ma <janay.ma7@gmail.com>
2023-05-17 14:05:25 -0700

    implement lotide assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,23 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  const assertion = actual === expected;
+  let msg = "Assertion ";
+  switch (assertion) {
+  case true:
+    msg += "Passed 💯💯 " + actual + " === " + expected;
+    break;
+  case false:
+    msg += "Failed ☠️☠️ " + actual + " !== " + expected;
+    break;
+  }
+  console.log(msg);
+};
+
+// TEST CODE
+
+assertEqual(1, 2);
+assertEqual("hello, world", "hello, world");
+assertEqual('hello, world', 'hello, planet');
+assertEqual(10.5, 10.5);
+assertEqual(true, true);
+assertEqual(true, false);
